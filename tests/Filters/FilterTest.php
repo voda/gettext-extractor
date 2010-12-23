2a7b7a8689859b738a13343683d63c2d483d27bd
Ondřej Vodáček <ondrej.vodacek@gmail.com>
2010-12-23 10:42:50 +0100

    added tests

---
+++ tests/Filters/FilterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+
+require_once dirname(__FILE__).'/../../Filters/AFilter.php';
+require_once dirname(__FILE__).'/../../Filters/iFilter.php';
+
+/**
+ * Description of FilterTest
+ *
+ * @author Ondřej Vodáček
+ */
+abstract class FilterTest extends PHPUnit_Framework_TestCase {
+
+	/** @var AFilter */
+	protected $object;
+
+	/** @var string */
+	protected $file;
+
+	public function testExtract() {
+		$messages = $this->object->extract($this->file);
+
+		$this->assertType('array', $messages);
+
+		$this->assertContains(array(
+			iFilter::LINE => 2,
+			iFilter::SINGULAR => 'A message!'
+		), $messages);
+
+		$this->assertContains(array(
+			iFilter::LINE => 3,
+			iFilter::SINGULAR => 'Another message!',
+			iFilter::CONTEXT => 'context'
+		), $messages);
+
+		$this->assertContains(array(
+			iFilter::LINE => 4,
+			iFilter::SINGULAR => 'I see %d little indian!',
+			iFilter::PLURAL => 'I see %d little indians!'
+		), $messages);
+
+		$this->assertContains(array(
+			iFilter::LINE => 5,
+			iFilter::SINGULAR => 'I see %d little indian!',
+			iFilter::PLURAL => 'I see %d little indians!',
+			iFilter::CONTEXT => 'context'
+		), $messages);
+	}
+    
+}
---
+++ tests/data/default.php
@@ -0,0 +1,20 @@
+<?php
+$translator->gettext('A message!');
+$translator->pgettext('context', 'Another message!');
+$translator->ngettext('I see %d little indian!', 'I see %d little indians!', 3);
+$translator->npgettext('context', 'I see %d little indian!', 'I see %d little indians!', $number);
+
+$translator->gettext($foo);
+$translator->pgettext('context', $bar);
+$translator->npgettext($baz, 'I see %d little indian!', 'I see %d little indians!', 3);
+
+$translator->gettext(someFunction('Some string.'));
+someFunction($translator->gettext('Nested function.'));
+$translator->gettext($translator->pgettext('context', 'Nested function 2.'));
+$list->add(Html::el('li')->setText(sprintf(_n("%d meeting wasn't imported.", "%d meetings weren't importeded.", $status->imported->size()), $status->imported->size())));
+
+$form->addRule(AppForm::FILLED, 'Please provide a text.');
+$form->addRule(getARule(), 'Please provide a text 2.');
+$form->addRule(getARule('a parameter'), 'Please provide a text 3.');
+
+$translator->gettext('A'.' message!');
