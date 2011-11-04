dd1fbdc6a95a219ed1164cc733a7204a2f9b6660
Ondřej Vodáček <ondrej.vodacek@gmail.com>
2011-11-04 16:14:23 +0100

    PHPUnit 3.6 compatibility

--- tests/Filters/AFilterTest.php
+++ tests/Filters/AFilterTest.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once 'PHPUnit/Framework.php';
-
 require_once dirname(__FILE__).'/../../Filters/AFilter.php';
 
 /**
--- tests/Filters/FilterTest.php
+++ tests/Filters/FilterTest.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once 'PHPUnit/Framework.php';
-
 require_once dirname(__FILE__).'/../../Filters/AFilter.php';
 require_once dirname(__FILE__).'/../../Filters/iFilter.php';
 
@@ -21,7 +19,7 @@ abstract class FilterTest extends PHPUnit_Framework_TestCase {
 	public function testExtract() {
 		$messages = $this->object->extract($this->file);
 
-		$this->assertType('array', $messages);
+		$this->assertInternalType('array', $messages);
 
 		$this->assertContains(array(
 			iFilter::LINE => 2,
