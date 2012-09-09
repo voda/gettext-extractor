df012e2f388cc79143919e75570fbeb5583a1270
Ondřej Vodáček <ondrej.vodacek@gmail.com>
2012-09-09 22:07:38 +0200

    set default charset to utf-8

--- GettextExtractor/Extractor.php
+++ GettextExtractor/Extractor.php
@@ -58,7 +58,7 @@ class GettextExtractor_Extractor {
 		"Last-Translator" => "FULL NAME <EMAIL@ADDRESS>",
 		"Language-Team" => "LANGUAGE <LL@li.org>",
 		"MIME-Version" => "1.0",
-		"Content-Type" => "text/plain; charset=CHARSET",
+		"Content-Type" => "text/plain; charset=UTF-8",
 		"Content-Transfer-Encoding" => "8bit",
 		"Plural-Forms" => "nplurals=INTEGER; plural=EXPRESSION;"
 	);
