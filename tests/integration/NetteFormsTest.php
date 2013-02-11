3ecca749c74de2019563cf6bda978891eab49de9
Ondřej Vodáček <ondrej.vodacek@gmail.com>
2013-02-11 15:47:00 +0100

    added integration tests

---
+++ tests/integration/NetteFormsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author Ondřej Vodáček
+ */
+class NetteFormsTest extends PHPUnit_Framework_TestCase {
+
+	/** @var GettextExtractor_NetteExtractor */
+	protected $object;
+
+	protected function setUp() {
+		$this->object = new GettextExtractor_NetteExtractor('/dev/null');
+		$this->object->setMeta('POT-Creation-Date', '2013-02-11T14:18:02+01:00');
+	}
+
+	public function testForm() {
+		$this->object->setupForms();
+		$this->object->scan('tests/integration/data/form.php');
+		$temp = tempnam(sys_get_temp_dir(), __CLASS__);
+		$this->object->save($temp);
+		$this->assertFileEquals(__DIR__.'/data/form.pot', $temp);
+	}
+}
