6878cfcdd79ab55ff1eeaf9d16937f8bab29a1d1
Ondrej Cech <ondrej.cech@gmail.com>
2020-01-30 12:43:28 +0100

    Tabs/spaces fixed]

--- tests/integration/NetteFormsTest.php
+++ tests/integration/NetteFormsTest.php
@@ -8,23 +8,23 @@ use Vodacek\GettextExtractor\NetteExtractor;
 
 class NetteFormsTest extends TestCase {
 
-    /** @var NetteExtractor */
-    protected $object;
+	/** @var NetteExtractor */
+	protected $object;
 
-    protected function setUp(): void {
-        $this->object = new NetteExtractor('/dev/null');
-        $this->object->setMeta('POT-Creation-Date', '2013-02-11T14:18:02+01:00');
-    }
+	protected function setUp(): void {
+		$this->object = new NetteExtractor('/dev/null');
+		$this->object->setMeta('POT-Creation-Date', '2013-02-11T14:18:02+01:00');
+	}
 
-    public function testForm(): void {
-        $this->object->setupForms();
-        $this->object->scan('tests/integration/data/form.php');
-        $temp = tempnam(sys_get_temp_dir(), __CLASS__);
-        if ($temp === false) {
-            throw new \RuntimeException('Failed to create temporary file.');
-        }
-        $this->object->save($temp);
-        self::assertFileEquals(__DIR__.'/data/form.pot', $temp);
-        unlink($temp);
-    }
+	public function testForm(): void {
+		$this->object->setupForms();
+		$this->object->scan('tests/integration/data/form.php');
+		$temp = tempnam(sys_get_temp_dir(), __CLASS__);
+		if ($temp === false) {
+			throw new \RuntimeException('Failed to create temporary file.');
+		}
+		$this->object->save($temp);
+		self::assertFileEquals(__DIR__.'/data/form.pot', $temp);
+		unlink($temp);
+	}
 }
